01b2e1779b1872bfb986b9a764dccaac5cf054bf
intrnl <intrnl28@gmail.com>
2018-06-26 10:29:27 +0700

    status

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,11 @@
 module.exports = async client => {
   console.log(`[${new Date().toISOString()}] Serving ${client.users.size} users in ${client.channels.size} channels across ${client.guilds.size} guilds.`)
   console.log(`[${new Date().toISOString()}] Ready.`)
+
+  client.user.setPresence({
+    status: 'online',
+    game: {
+      name: `${client.commandPrefix}`
+    }
+  })
 }
